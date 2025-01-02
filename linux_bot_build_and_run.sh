22cf5dc33cdcfd8d4664516d5cfa568f55a848aa
W1ndys <w1ndys@outlook.com>
2025-01-02 18:04:37 +0800

    feat: 删除构建和运行 Docker 容器的脚本文件

--- linux_bot_build_and_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 构建 Docker 镜像
-docker build -t bot .
-
-# 运行 Docker 容器，并映射主机的 3001 端口到容器的 3001 端口
-docker run -d -v $(pwd)/app:/app --name bot -p 3001:3001 bot
--- linux_run_bot_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# 删除已有的容器
-docker rm -f bot
-
-# 运行容器
-docker run -d -v $(pwd)/app:/app --name bot -p 3001:3001 bot
