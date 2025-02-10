20632abbea5849f69c5f4eceada477ed15a3a55e
W1ndys <w1ndys@outlook.com>
2025-02-10 10:13:26 +0800

    Update config.py
    默认端口改为3001

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ owner_id = ["x"]  # 机器人root管理员 QQ 号
 
 report_group_id = "x"  # 上报群
 
-ws_url = "ws://127.0.0.1:3000"  # 本地环境的 WebSocket API 地址
+ws_url = "ws://127.0.0.1:3001"  # 本地环境的 WebSocket API 地址
 
 token = None  # 如果需要认证，请填写认证 token
