de32ccdeaa1171c0b2dc694840952c6f44cec019
W1ndys <w1ndys@outlook.com>
2025-02-22 23:48:06 +0800

    feat 新增钉钉配置

--- app/config.py
+++ app/config.py
@@ -7,3 +7,7 @@ report_group_id = "x"  # 上报群
 ws_url = "ws://127.0.0.1:3001"  # 本地环境的 WebSocket API 地址
 
 token = None  # 如果需要认证，请填写认证 token
+
+DD_BOT_TOKEN = "dingtalk_token"
+
+DD_BOT_SECRET = "dingtalk_secret"
